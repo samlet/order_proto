8e1e1f201d72feca4fc16e0f24078f13622b1ab9
Samlet Wu <xiaofei.wu@gmail.com>
2021-11-04 17:13:12 +0800

    add function as http-api

--- src/main/java/com/bluecc/bluesrv/ecomm/EcommApp.java
+++ src/main/java/com/bluecc/bluesrv/ecomm/EcommApp.java
@@ -1,8 +1,15 @@
 package com.bluecc.bluesrv.ecomm;
 
+import lombok.Data;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /*
 $ just app ecomm.EcommApp
@@ -13,6 +20,76 @@ $ just app ecomm.EcommApp
 public class EcommApp {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommApp.class, args);
+	}
+
+	@Autowired
+	Feeder feeder;
+
+	@Bean
+	public Function<RetriveNumber, List<Feeder.Contract>> contractSeeds(){
+		return num -> feeder.genPostalAddress(num.total);
+	}
+
+	@Bean
+	public Function<Foo, Bar> uppercase() {
+		return value -> new Bar(value.uppercase());
+	}
+
+}
+
+// @checkstyle:on
+
+@Data
+class RetriveNumber{
+	int total;
+}
+
+class Foo {
+
+	private String value;
+
+	Foo() {
+	}
+
+	Foo(String value) {
+		this.value = value;
+	}
+
+	public String lowercase() {
+		return this.value.toLowerCase();
+	}
+
+	public String uppercase() {
+		return this.value.toUpperCase();
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+}
+
+class Bar {
+
+	private String value;
+
+	Bar() {
+	}
+
+	Bar(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
 	}
 
 }
