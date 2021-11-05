70871e4c06459f453347f28604d64fa6e50896dc
Samlet Wu <xiaofei.wu@gmail.com>
2021-11-05 14:07:46 +0800

    add LocalDateTimeAdapter

--- src/main/java/com/bluecc/bluesrv/common/Helper.java
+++ src/main/java/com/bluecc/bluesrv/common/Helper.java
@@ -1,14 +1,55 @@
 package com.bluecc.bluesrv.common;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static com.google.gson.FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES;
 
 public class Helper {
+//    static class LocalDateAdapter implements JsonSerializer<LocalDate> {
+//        public JsonElement serialize(LocalDate date, Type typeOfSrc, JsonSerializationContext context) {
+//            return new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE)); // "yyyy-mm-dd"
+//        }
+//    }
+//
+//    static class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+//        public JsonElement serialize(LocalDateTime date, Type typeOfSrc, JsonSerializationContext context) {
+//            return new JsonPrimitive(date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)); // "yyyy-mm-ddTxxxxxx"
+//        }
+//        @Override
+//        public LocalDateTime deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+//            return null;
+//        }
+//    }
+
+    /**
+     * A simpler implementation. Add null support to by registering the nullSafe() wrapped version
+     * ref: https://stackoverflow.com/questions/39192945/serialize-java-8-localdate-as-yyyy-mm-dd-with-gson
+     */
+    private static final class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+        @Override
+        public void write(final JsonWriter jsonWriter, final LocalDateTime localDate ) throws IOException {
+            jsonWriter.value(localDate.toString());
+        }
+
+        @Override
+        public LocalDateTime read( final JsonReader jsonReader ) throws IOException {
+            return LocalDateTime.parse(jsonReader.nextString());
+        }
+    }
+
     public static final Gson GSON = new GsonBuilder()
             .setFieldNamingPolicy(LOWER_CASE_WITH_UNDERSCORES)
             .setDateFormat("yyyy-MM-dd HH:mm:ss")
+//            .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter().nullSafe())
             .setPrettyPrinting()
             .create();
 }
--- src/test/java/com/bluecc/bluesrv/common/JsonSerializeTest.java
+++ src/test/java/com/bluecc/bluesrv/common/JsonSerializeTest.java
@@ -1,8 +1,10 @@
 package com.bluecc.bluesrv.common;
 
+import com.google.gson.Gson;
 import lombok.Data;
 import org.junit.jupiter.api.Test;
 
+import java.time.*;
 import java.util.Date;
 
 import static com.bluecc.bluesrv.common.Helper.GSON;
@@ -12,9 +14,61 @@ public class JsonSerializeTest {
     static class Stuff{
         Date birthday=new Date();
     }
+    @Data
+    static class DateStuff{
+        LocalDateTime birthday= LocalDateTime.now();
+    }
 
     @Test
     void testDate(){
         System.out.println(GSON.toJson(new Stuff()));
+        System.out.println(GSON.toJson(new DateStuff()));
+        DateStuff ds=GSON.fromJson("{\"birthday\": \"2021-11-05T13:57:10.862\"}", DateStuff.class);
+        System.out.println(ds);
+
+        Gson gson=new Gson(); // with the original one
+        String dateJson="{\n" +
+                "  \"birthday\": {\n" +
+                "    \"date\": {\n" +
+                "      \"year\": 2021,\n" +
+                "      \"month\": 11,\n" +
+                "      \"day\": 5\n" +
+                "    },\n" +
+                "    \"time\": {\n" +
+                "      \"hour\": 13,\n" +
+                "      \"minute\": 22,\n" +
+                "      \"second\": 52,\n" +
+                "      \"nano\": 877000000\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        DateStuff ds1=gson.fromJson(dateJson, DateStuff.class);
+        System.out.println(ds1);
+    }
+
+    /*
+    https://stackoverflow.com/questions/39628885/how-convert-localdatetime-to-date-in-java-8
+    http://www.world-timedate.com/timezone/timezone_info_by_country.php?country_id=70
+     */
+    @Test
+    void testInstant(){
+        ZoneId zNewYork = ZoneId.of("America/New_York");
+        ZoneId zShanghai = ZoneId.of("Asia/Shanghai");
+
+        Instant now = Instant.now();
+        ZonedDateTime zdtNewYork = now.atZone( zNewYork );
+        ZonedDateTime zdtShanghai = now.atZone( zShanghai );
+
+//        Instant instant = zdtNewYork.toInstant();
+        System.out.println(now);
+        System.out.println(zdtNewYork.toLocalDateTime());
+        System.out.println(zdtShanghai.toLocalDateTime());
+    }
+
+    @Test
+    void testLocalDateTime(){
+        LocalDateTime dateTime = LocalDateTime.of(2017, Month.JUNE, 15, 13, 39);
+        Instant instant = dateTime.atZone(ZoneId.of("Europe/Paris")).toInstant();
+        System.out.println(instant); // 2017-06-15T11:39:00Z
     }
 }
